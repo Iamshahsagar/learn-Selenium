e4cab14fc26e539bdb8d909ef89c0ff9d5dc97de
Sagar Shah <iamshahsagar@gmail.com>
2020-02-14 19:22:04 +0530

    Auto

--- src/Assignments/grofers.java
+++ src/Assignments/grofers.java
@@ -1,32 +1,14 @@
 package Assignments;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FilterInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import javax.imageio.stream.FileImageInputStream;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import java.util.List;
 
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
 
 public class grofers {
 	public static void main(String[] args) throws IOException, InterruptedException {
@@ -35,9 +17,9 @@ public class grofers {
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver(options);
 		
-		driver.get("https://grofers.com/");
+		driver.get("https://sheet1ers.com/");
 		driver.manage().window().maximize();
-		
+		Xls_Reader datatable = null;
 		
 	//	FileInputStream file = new FileInputStream("C:\\\\Users\\\\dhaval\\\\eclipse-workspace\\\\Test\\\\src\\\\Work\\\\Testing.xlsx");
 	//	XSSFWorkbook workbook = new XSSFWorkbook(file);
@@ -68,37 +50,39 @@ public class grofers {
 		Thread.sleep(5000);
 		
 		
-		/*Xls_Reader reader = new Xls_Reader("C:\\Users\\dhaval\\eclipse-workspace\\Test\\src\\Work\\Testing.xlsx");
-		reader.addSheet("Grof");
-		reader.addcolumn("Grof", "Product Name");
-		reader.addcolumn("Grof", "Product Quantity");
-		reader.addcolumn("Grof", "Product Price");
-		*/
+		Xls_Reader reader = new Xls_Reader("C:\\Users\\dhaval\\eclipse-workspace\\Test\\src\\Work\\Testing.xlsx");
+		reader.addSheet("sheet1");
+		reader.addColumn("sheet1", "Product Name");
+		reader.addColumn("sheet1", "Product Quantity");
+		reader.addColumn("sheet1", "Product Price");
+		
 		
 		for(int i=0;i<=list.size();i++)
 		{
 			System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
 			String proname = pn.get(i).getText();
 		
-			//reader.setCellData("Grof", "Product Name", i , "proname");
+			reader.setCellData("sheet1", "Product Name", i , "proname");
 			
 			String proQ = pw.get(i).getText();
 			
-		//	reader.setCellData("Grof", "Product Quantity", i , "proQ");
+			reader.setCellData("sheet1", "Product Quantity", i , "proQ");
 			
 			String propri = pnp.get(i).getText();
+	
+			reader.setCellData("sheet1", "Product Price", i , "propri");
 			
-		//	reader.setCellData("Grof", "Product Price", i , "propri");
 			
+		sheet1.getRow(i).createCell(0).setCellValue(proname);
+		sheet1.getRow(i).createCell(1).setCellValue(proQ);
+		sheet1.getRow(i).createCell(2).setCellValue(propri);
 			
 			
 			System.out.println("Product name :" + proname);
 			System.out.println("Product Quantity :" + proQ);
 			System.out.println("Product price :" + propri);
 		}
-	//		sheet.getRow(i).createCell(0).setCellValue(proname);
-	//		sheet.getRow(i).createCell(1).setCellValue(proQ);
-		//	sheet.getRow(i).createCell(2).setCellValue(propri);
+			
 			
 			//System.out.println(list.get(i).getText());
 				list.get(6).click();
